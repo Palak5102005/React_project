8b2cd7f72400b37c0f5c47bb38e7e906a3944fe5
Palak5102005 <singhalpalak086@gmail.com>
2025-09-07 16:58:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Expensetracker from './Projects/expense_tracker/expensetracker.js'
+
+function App() {
+  return (
+    <div>
+      <Expensetracker/>
+
+    </div>
+  )
+}
+
+export default App
